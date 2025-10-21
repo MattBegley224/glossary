70256fa544d83463421addb9eb30b59899bde42e
Matt B <mattbegley224@gmail.com>
2025-10-20 23:54:57 -0300

    Updated IntroVideo.tsx

--- components/IntroVideo.tsx
+++ components/IntroVideo.tsx
@@ -37,7 +37,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    backgroundColor: '#000',
+    backgroundColor: '#fff',
   },
   video: {
     alignSelf: 'center',
